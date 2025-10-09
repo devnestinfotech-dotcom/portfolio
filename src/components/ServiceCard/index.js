404d39ace7cb9bb4684552bc7d204d22377c7482
HitenSavaliyaLabra <hiten.s@labra.io>
2025-10-09 19:08:17 +0530

    portfolio app

--- src/components/ServiceCard/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Card, Description, IconBox, Title } from './index.styles';
-export const ServiceCard = ({ icon, title, description, }) => {
-    return (_jsxs(Card, { height: '100%', children: [_jsx(IconBox, { children: icon }), _jsx(Title, { children: title }), _jsx(Description, { children: description })] }));
-};
